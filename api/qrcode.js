03dd72c081277414c444d22ffc3cd4d485bfae84
felix <felix.liu@foxmail.com>
2018-01-20 19:54:42 +0800

    update qrcode

--- api/qrcode.js
+++ api/qrcode.js
@@ -11,20 +11,19 @@ router.post('/', function (req, res) {
     let userid1 = req.body.userid1;
 
 
-    let scene=userid1;
-    log.debug("scene:"+scene);
-    if(scene.length>32)
-    {
-        log.error("scene:"+scene);
+    let scene = userid1;
+    log.debug("scene:" + scene);
+    if (scene.length > 32) {
+        log.error("scene:" + scene);
         response.push({
             'result': 'error',
-            'scene': 'scene is too long :'+scene.length +":"+scene
+            'scene': 'scene is too long :' + scene.length + ":" + scene
         });
         res.status(200).send(JSON.stringify(response));
         return;
     }
-    let qrcodeFullPath=scene+".png";
-    let qrcodeURL=getQRCodeURL(qrcodeFullPath);
+    let qrcodeFullPath = scene + ".png";
+    let qrcodeURL = getQRCodeURL(qrcodeFullPath);
 
 
     access_token((accessToken) => {
@@ -47,25 +46,32 @@ router.post('/', function (req, res) {
 
             body: {
                 'scene': scene,
-                'page':'page/booking/qrBookingNew'
+                'page': 'page/booking/qrBookingNew'
 
             }
         };
         var qrCodePath = getQRCodePath(qrcodeFullPath);
+        fs.exists(qrCodePath, function (exists) {
+            if (!exists) {
+                var writeStream = fs.createWriteStream(qrCodePath, {autoClose: true});
 
-        var writeStream=fs.createWriteStream(qrCodePath,{autoClose:true});
+                request(options).pipe(writeStream);
+                writeStream.on('finish', function () {
 
-        request(options).pipe(writeStream);
-
-        writeStream.on('finish',function(){
-
-            response.push({
-                'result': 'success',
-                'qrcode': qrcodeURL
-            });
-            res.status(200).send(JSON.stringify(response));
+                    response.push({
+                        'result': 'success',
+                        'qrcode': qrcodeURL
+                    });
+                    res.status(200).send(JSON.stringify(response));
+                })
+            } else {
+                response.push({
+                    'result': 'success',
+                    'qrcode': qrcodeURL
+                });
+                res.status(200).send(JSON.stringify(response));
+            }
         })
-
 
 
     });
@@ -75,11 +81,11 @@ router.post('/', function (req, res) {
 
 
 function getQRCodePath(imgName) {
-    return "./public/qr/" + imgName;
+    return "/home/felix/www-booking/public/qr/" + imgName;
 }
 
 function getQRCodeURL(imgName) {
-    return "http://localhost:8081/qr/" + imgName;
+    return "https://www.4exam.cn/qr/" + imgName;
 }
 
 
