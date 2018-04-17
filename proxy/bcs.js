31d657e85ba6cae80b08c31c2786ba8cb21e3bc9
felix <felix.liu@foxmail.com>
2018-04-17 10:23:51 +0800

    add bcs serverinfo

--- proxy/bcs.js
+++ proxy/bcs.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
 var log = require('log4js').getLogger("api/jf_vendor");
 log.level = "debug";
@@ -13,13 +14,31 @@ router.get('/getLedgerInfo', function (req, res, next) {
         "deployment": 2,
         "invokes": 293
     }
-    response.push({
-        'result': 'success',
-        'data': data
+
+    let options = {
+        method: 'get',
+        url: "http://129.157.179.22:3011/api/v1/channels/appdev1orderer/summary?peerAddr=peer0.appdev1.oracle.com&peerPort=7051&filterby=all",
+        json: true,
+        headers: {
+            "content-type": "application/json",
+        },
+
+        body: {
+        }
+    };
+
+    request(options, function (error, resBcs, body) {
+        if(resBcs.statusCode===200){
+            data=body;
+        }
+        response.push({
+            'result': 'success',
+            'data': data
+        });
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(JSON.stringify(response));
     });
 
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).send(JSON.stringify(response));
 });
 
 router.get('/getServerInfo', function (req, res, next) {
